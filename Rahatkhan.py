4c87ad3d97b9b9354d6fed817711df54a4c92b7e
Janrahat <111644390+Janrahat@users.noreply.github.com>
2023-04-14 03:06:52 +0500

    Create Rahatkhan.py

---
+++ Rahatkhan.py
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+C="\e[1;36m"
+
+R="\e[1;31m"
+
+E="\e[0m"
+
+G='\033[1;32m'
+
+echo -e "$C
+
+               ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+
+               ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+
+               ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+
+               ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+
+               ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+
+                ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+
+$E"
+
+echo -e "$R                               Coded By$G.  Rahat khan     $E"
+
+echo -e "$C                            Website:- $R www.Rahatkhan.rk  $E"
+
+figlet -f big Installing | lolcat
+
+pkg update && pkg upgrade
+
+apt update && apt upgrade
+
+apt install curl -y
+
+apt install wget -y
+
+apt install python -y
+
+apt install python2 -y
+
+apt install bash -y
+
+apt install cowsay -y
+
+apt install git -y
+
+apt install dart -y
+
+apt install toilet -y
+
+apt install fish -y
+
+apt install figlet -y
+
+apt install php -y
+
+apt install php -y
+
+apt install ruby -y
+
+apt install perl -y
+
+apt install clang -y
+
+apt install macchanger -y
+
+apt install jq -y
+
+apt install tor -
+
+pkg install openssh -y
+
+apt install openssly
+
+apt-get update -y
+
+figlet -f big Done!!! | lolcat
+
+figlet -f standard Thank for using... | lolcat
+
